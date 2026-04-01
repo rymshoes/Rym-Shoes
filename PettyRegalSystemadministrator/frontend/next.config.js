a2af7307c1553e37706638bb59321c73c785370a
rymshoes <zbzb20287@gmail.com>
2026-04-01 21:50:58 +0100

    Update next.config.js

--- PettyRegalSystemadministrator/frontend/next.config.js
+++ PettyRegalSystemadministrator/frontend/next.config.js
@@ -1,15 +1,23 @@
 /** @type {import('next').NextConfig} */
-const BACKEND = process.env.BACKEND_URL || 'http://localhost:3001';
+
+const BACKEND = process.env.NEXT_PUBLIC_API_URL;
 
 const nextConfig = {
   reactStrictMode: true,
+
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: '**' },
       { protocol: 'http', hostname: '**' },
     ],
   },
+
   async rewrites() {
+    if (!BACKEND) {
+      console.warn("⚠️ NEXT_PUBLIC_API_URL not defined");
+      return [];
+    }
+
     return [
       {
         source: '/api/:path*',
