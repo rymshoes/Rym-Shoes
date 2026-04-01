921957f20e35ffc65974af6e52f7f85731291bc3
rymshoes <zbzb20287@gmail.com>
2026-04-02 00:28:20 +0100

    Update auth.js

--- PettyRegalSystemadministrator/backend/routes/auth.js
+++ PettyRegalSystemadministrator/backend/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const pool = require('../db');
-const { JWT_SECRET } = require('../middleware/auth');
+const JWT_SECRET = process.env.JWT_SECRET || 'secret123';
 
 const router = express.Router();
 
